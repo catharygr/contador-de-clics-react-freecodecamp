bf31b7fc7f264f1af12e52e31f2624906417068e
cathy <cathy@bubulazi.com>
2022-12-29 18:17:24 +0000

    Agregados los botones den contador

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css'
 import freCodecamp from './imagenes/freecodecamp-logo.png'
+import Boton from './componentes/Boton'
 
 function App() {
+
+  const manejarClic = () => {
+    console.log('clic')
+  }
+
+  const reiniciarContador = () => {
+      console.log('Reiniciar')
+  }
   return (
     <div className="App">
       <div className='freecodecamp-logo-contenedor'>
@@ -11,7 +20,18 @@ function App() {
           alt='Logo de freeCodecamp'
         />
       </div>
-        <div className='contenedor-principal'></div>
+        <div className='contenedor-principal'>
+    <Boton
+      texto='Clic'
+      esBotonDeClic={true}
+      manejarClic={manejarClic} />
+
+    <Boton
+        texto='Reiniciar'
+        esBotonDeClic={false}
+        manejarClic={reiniciarContador} />
+
+        </div>
     </div>
   );
 }
--- src/componentes/Boton.js
+++ src/componentes/Boton.js
@@ -1,5 +1,11 @@
-import React from 'react';
+import React from 'react'
 
-function Boton() {
-  
+export default function Boton({ texto, esBotonDeClic, manejarClic}) {
+  return(
+    <button 
+    className={esBotonDeClic ? 'boton-clic' : 'boton-reiniciar'}
+      onClick={manejarClic}>
+      {texto}
+    </button>
+  )
 }
